785337424065e40fcc6eb393138d7ed18ff1bc4b
dongnguyen248 <dacdongdhv@gmail.com>
2020-10-14 14:14:44 +0700

    edit authencontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,25 +33,7 @@ class AuthController extends Controller
 
         return $this->respondWithToken($token);
     }
-    public function register(Request $request)
-    {
-        $v = Validator::make($request->all(), [
-            'email' => 'required|email|unique:users',
-            'password'  => 'required|min:3|confirmed',
-        ]);
-        if ($v->fails()) {
-            return response()->json([
-                'status' => 'error',
-                'errors' => $v->errors()
-            ], 422);
-        }
-        $user = new User;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->save();
-        return response()->json(['status' => 'success'], 200);
-    }
-
+   
     /**
      * Get the authenticated User.
      *
